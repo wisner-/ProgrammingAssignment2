db29d739bb3d3625711b60e8d6481a46e667646c
andrew wisner <wisner@rocketmail.com>
2014-08-22 15:19:37 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+## Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
 
 }
